b94cfefdb74c7e3e2bd6e05bc3ab9fdac2facb51
protoy <37529943+protoy@users.noreply.github.com>
2020-04-23 23:52:47 +0800

    gallery JS

--- gallery/script/showPic.js
+++ gallery/script/showPic.js
@@ -2,13 +2,35 @@ function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
+    if(document.getElementById("description")){
+        var text = whichpic.getAttribute("title");
+        var description = document.getElementById("description");
+        description.firstChild.nodeValue = text;
+    }
+    return true;
+}
+function prepareGallery() {
+    if(!document.getElementsByTagName || !document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0; i < links.length; ++i){
+        links[i].onclick = function() {
+            return !showPic(this);
+        }
+    }
+}
 
-    var text = whichpic.getAttribute("title");
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function') {
+        window.onload = func;
+    }else{
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
 }
-function countBodyChildren() {
-    var body_element = document.getElementsByTagName("body")[0];
-    alert(body_element.childNodes.length);
-}
-window.onload = countBodyChildren;
+addLoadEvent(prepareGallery);
+addLoadEvent(showPic);
