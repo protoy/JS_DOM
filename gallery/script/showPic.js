a5a8106f386642cc5b35c0d4245ca23daa2d778f
protoy <37529943+protoy@users.noreply.github.com>
2020-04-25 23:22:16 +0800

    prepareplaceholder

--- gallery/script/showPic.js
+++ gallery/script/showPic.js
@@ -1,14 +1,41 @@
-function showPic(whichpic) {
-    var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src", source);
-    if(document.getElementById("description")){
-        var text = whichpic.getAttribute("title");
-        var description = document.getElementById("description");
-        description.firstChild.nodeValue = text;
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function') {
+        window.onload = func;
+    }else{
+        window.onload = function() {
+            oldonload();
+            func();
+        }
     }
-    return true;
 }
+
+function insertAfter(newElement, targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChile == targetElement) {
+        parent.appendChild(newElement);
+    }else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function prepareplaceholder() {
+    var placeholder = document.createElement("img");
+    var description = document.createElement("p");
+    var desctext = document.createTextNode("Choose an image");
+    var gallery = document.getElementById("imagegallery");
+
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.png");
+    placeholder.setAttribute("alt", "my image gallery")
+
+    description.setAttribute("id", "description");
+    description.appendChild(desctext);
+
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+}
+
 function prepareGallery() {
     if(!document.getElementsByTagName || !document.getElementById) return false;
     if(!document.getElementById("imagegallery")) return false;
@@ -21,16 +48,16 @@ function prepareGallery() {
     }
 }
 
-function addLoadEvent(func){
-    var oldonload = window.onload;
-    if(typeof window.onload != 'function') {
-        window.onload = func;
-    }else{
-        window.onload = function() {
-            oldonload();
-            func();
-        }
+function showPic(whichpic) {
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src", source);
+    if(document.getElementById("description")){
+        var text = whichpic.getAttribute("title");
+        var description = document.getElementById("description");
+        description.firstChild.nodeValue = text;
     }
+    return true;
 }
 addLoadEvent(prepareGallery);
-addLoadEvent(showPic);
+addLoadEvent(prepareplaceholder);
